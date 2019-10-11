28978bd0f287dbced494ded9ef1917c786bc026b
Lucas362 <lucasvitorifg@gmail.com>
2019-10-11 12:09:38 -0300

    Adding Paris
    Co-authored-by: Rafael Teodosio <rafzteodosio@gmail.com>

---
+++ Cities/Paris.pl
@@ -0,0 +1,64 @@
+
+%Accommodation
+bed_and_breakfast("Le Bristol Paris", true).
+bed_and_breakfast("La Chambre du Marais", true).
+hotel("Le Bristol Paris", true).
+hotel("La Chambre du Marais", true).
+
+%AccommodationRating
+one_s("Le Bristol Paris", false).
+one_s("La Chambre du Marais", false).
+two_s("Le Bristol Paris", false).
+two_s("La Chambre du Marais", false).
+three_s("Le Bristol Paris", true).
+three_s("La Chambre du Marais", true).
+
+%Activity
+
+%Adventure
+bunjee_jumping("Le Bristol Paris", false).
+bunjee_jumping("La Chambre du Marais", false).
+adv_safari("Le Bristol Paris", false).
+adv_safari("La Chambre du Marais", false).
+
+%Relaxation
+sunbathing("Le Bristol Paris", false).
+sunbathing("La Chambre du Marais", false).
+yoga("Le Bristol Paris", true).
+yoga("La Chambre du Marais", true).
+
+%Sightseeing
+museums("Le Bristol Paris", true).
+museums("La Chambre du Marais", true).
+s_safari("Le Bristol Paris", false).
+s_safari("La Chambre du Marais", false).
+
+%Sports
+hiking("Le Bristol Paris", false).
+hiking("La Chambre du Marais", false).
+surfing("Le Bristol Paris", false).
+surfing("La Chambre du Marais", false).
+
+%Contact
+contact("Le Bristol Paris", true, "+33 1 86 76 10 75").
+contact("La Chambre du Marais", true, "+33 1 86 76 10 75").
+
+%Destination
+beach("Le Bristol Paris", false).
+beach("La Chambre du Marais", false).
+
+%RuralArea
+
+%FarmLand
+farm_land("Le Bristol Paris", false).
+farm_land("La Chambre du Marais", false).
+
+%NationalPark
+national_park("Le Bristol Paris", false).
+national_park("La Chambre du Marais", false).
+
+%UrbanArea
+
+%Town
+town("Le Bristol Paris", true).
+town("La Chambre du Marais", true).
