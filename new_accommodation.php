7d47f57e9f1d5ca038c8a9fa91e4b73b202100e3
Pedro Igor <pedroigor.silva@outlook.com>
2019-10-14 23:12:25 -0300

    Erro ao mostar dynamic country.

--- new_accommodation.php
+++ new_accommodation.php
@@ -4,21 +4,10 @@ if($_POST){
     $city = $_POST['ac-city'];
     $contact = $_POST['ac-contact'];
     $image = $_POST['ac-image'];
-    exec('swipl -s Accommodation.pl -g "assert(name('."'".$name."',".$city.",'".'true'."'".')), halt"', $output);
-    exec('swipl -s Accommodation.pl -g "assert(city('."'".$name."',".$city.",'".'true'."'".')), halt"', $output);
-    exec('swipl -s Accommodation.pl -g "assert(contact('."'".$name."',".$city.",'".'true'."'".')), halt"', $output);
-    exec('swipl -s Accommodation.pl -g "assert(image('."'".$name."',".$city.",'".$image."'".')), halt"', $output);
-
-    exec('swipl -s Accommodation.pl -g "assert(one_s('."'".$name."',".$city.",'".'true'."'".')), halt"', $output);
-    exec('swipl -s Accommodation.pl -g "assert(two_s('."'".$name."',".$city.",'".'true'."'".')), halt"', $output);
-    exec('swipl -s Accommodation.pl -g "assert(three_s('."'".$name."',".$city.",'".'true'."'".')), halt"', $output);
-
-
-
-
-
-
-
-
+    exec('swipl -s Accommodation.pl -g "assert_contact('."' ".$name." ',".$city.",' ".$contact." '".'), halt"', $output);
+    exec('swipl -s Accommodation.pl -g "assert_image('."' ".$name." ',".$city.",' ".$image." '".'), halt"', $output);
+    exec('swipl -s Accommodation.pl -g "assert_one_s('."' ".$name." ',".$city.",".'true'.'), halt"', $output);
+    exec('swipl -s Accommodation.pl -g "assert_two_s('."' ".$name." ',".$city.",".'true'.'), halt"', $output);
+    exec('swipl -s Accommodation.pl -g "assert_three_s('."' ".$name." ',".$city.",".'true'.'), halt"', $output);
 }
 ?>
